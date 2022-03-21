73be6f1b50b660fc0a2ed6886f7ab8fe74fd523d
Evgeny Zbirun <zbirunzbirun@gmail.com>
2022-03-21 13:46:11 -0700

    add Fahrenheit conversion
    co-authored-by: Jake Edgar <jakeedgar1012@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,45 @@
-function add(number1, number2) {
-  return number1 + number2;
+// function add(number1, number2) {
+//   return number1 + number2;
+// }
+
+// const number1 = parseInt(prompt("Enter a number:"));
+// const number2 = parseInt(prompt("Enter another number:"));
+// const result = add(number1, number2);
+// alert(result);
+
+// function subtract(number1, number2) {
+//   return number1 - number2;
+// }
+
+// const number1 = parseInt(prompt("Enter a number:"));
+// const number2 = parseInt(prompt("Enter another number:"));
+// const result = subtract(number1, number2);
+// alert(result);
+
+// function multiply(number1, number2) {
+//   return number1 * number2;
+// }
+
+// const number1 = parseInt(prompt("Enter a number:"));
+// const number2 = parseInt(prompt("Enter another number:"));
+// const result = multiply(number1, number2);
+// alert(result);
+
+// function divide(number1, number2) {
+//   return number1 / number2;
+// }
+
+// const number1 = parseInt(prompt("Enter a number:"));
+// const number2 = parseInt(prompt("Enter another number:"));
+// const result = divide(number1, number2);
+// alert(result);
+
+
+function temp(number1) {
+  return (number1 - 32) * (5 / 9);
 }
 
-const number1 = parseInt(prompt("Enter a number:"));
-const number2 = parseInt(prompt("Enter another number:"));
-const result = add(number1, number2);
+const number1 = parseInt(prompt("Enter a temperature in Farenheit:"));
+const result = temp(number1);
 alert(result);
+
